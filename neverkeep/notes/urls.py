7588b96ac1fdb72090d4c5aa66b4275073824fbd
melvinpetix <melvinbiscarra25@gmail.com>
2020-09-03 00:03:29 +0800

    first revision

--- neverkeep/notes/urls.py
+++ neverkeep/notes/urls.py
@@ -1,4 +1,4 @@
-"""notekeeper URL Configuration
+"""neverkeep URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
